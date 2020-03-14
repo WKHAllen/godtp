69fcfe6418ae5e13ea78cd179c602ed499adc4e4
WKHAllen <wkhallen@gmail.com>
2020-03-13 23:58:59 -0500

    Add function to remove clients

--- server.go
+++ server.go
@@ -110,19 +110,6 @@ func (server *Server) Serving() bool {
 	return server.serving
 }
 
-// parseAddr parses an address
-func (server *Server) parseAddr(addr string) (string, uint16, error) {
-	index := strings.LastIndex(addr, ":")
-	if index > -1 {
-		port, err := strconv.Atoi(addr[index + 1:])
-		if err == nil {
-			return addr[:index], uint16(port), nil
-		}
-		return "", 0, fmt.Errorf("Port conversion failed")
-	}
-	return "", 0, fmt.Errorf("No port found")
-}
-
 // GetAddr returns the address string
 func (server *Server) GetAddr() (string, uint16, error) {
 	return server.parseAddr(server.sock.Addr().String())
@@ -136,7 +123,30 @@ func (server *Server) GetClientAddr(clientID uint) (string, uint16, error) {
 	return "", 0, fmt.Errorf("Client does not exist")
 }
 
+// RemoveClient disconnects a client from the server
+func (server *Server) RemoveClient(clientID uint) error {
+	if client, ok := server.clients[clientID]; ok {
+		client.Close()
+		delete(server.clients, clientID)
+		return nil
+	}
+	return fmt.Errorf("Client does not exist")
+}
+
 // Serve clients
 func (server *Server) serve() {
 	defer server.wg.Done()
+}
+
+// parseAddr parses an address
+func (server *Server) parseAddr(addr string) (string, uint16, error) {
+	index := strings.LastIndex(addr, ":")
+	if index > -1 {
+		port, err := strconv.Atoi(addr[index + 1:])
+		if err == nil {
+			return addr[:index], uint16(port), nil
+		}
+		return "", 0, fmt.Errorf("Port conversion failed")
+	}
+	return "", 0, fmt.Errorf("No port found")
 }
